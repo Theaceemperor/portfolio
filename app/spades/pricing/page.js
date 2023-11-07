9eec1b2cd275076b8f47232cc251d00357787455
Theaceemperor <omoobajesuexcellent@gmail.com>
2023-11-07 23:16:18 +0100

    pricing image

--- app/spades/pricing/page.js
+++ app/spades/pricing/page.js
@@ -3,13 +3,14 @@ import Image from "next/image";
 export default function Page() {
 
     return (
-        <Image 
-        src={'/img/invoice.jpg'}
-        alt="Pricing Image"
-        width={920}
-        height={1020}
-        className="h-auto w-full px-10 my-10"
-        priority={true}
-        />
+        <div>
+            <Image 
+            src={'/img/invoice.jpg'}
+            alt="Pricing Image"
+            width={920}
+            height={1020}
+            className="h-auto w-full px-10 my-10"
+            />
+        </div>
     )
 }
