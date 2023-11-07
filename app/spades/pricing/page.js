bece5ea61b04450c2f3210b818c22b89195f0f96
Theaceemperor <omoobajesuexcellent@gmail.com>
2023-11-07 21:21:53 +0100

    added pricing page

---
+++ app/spades/pricing/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+
+export default function Page() {
+
+    return (
+        <Image 
+        src={'/img/invoice.jpg'}
+        alt="Pricing Image"
+        width={920}
+        height={1020}
+        className="h-auto w-full px-10 my-10"
+        priority={true}
+        />
+    )
+}
